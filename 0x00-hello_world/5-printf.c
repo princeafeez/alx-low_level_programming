d408af018ae8bdf4fcce6eae1aedb85ef2048437
OYEDEJI, Afeez Abidemi <afeezoyedeji@gmail.com>
2022-10-17 04:23:01 -0700

    second

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- entry point for the program.
+ *
+ * Return:0 if no error and returns non zero if error
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
