8369493c5c361210cedf06e7b115f8b30313242f
OYEDEJI, Afeez Abidemi <afeezoyedeji@gmail.com>
2022-10-17 04:43:06 -0700

    third

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- *main- Serves as the entry point for the program
+ * main- Serves as the entry point for the program
  *
- *Return: By default zero if no error otherwise returns non-zero value.
+ * Return: By default zero if no error otherwise returns non-zero value.
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
