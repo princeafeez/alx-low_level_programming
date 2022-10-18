7cf87de6aa91c3a5ebfbc80b4301a9511c32ed0d
OYEDEJI, Afeez Abidemi <afeezoyedeji@gmail.com>
2022-10-18 08:07:30 -0700

    second commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- is the starting point of the program
+ *
+ * Return: 0 when no error and otherwise when there is error
+ */
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
+	return (0);
+}
